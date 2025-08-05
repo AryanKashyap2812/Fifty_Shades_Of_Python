c76ab591ca6b0fa4d3cd8781902ad4991a4d751d
AryanKashyap2812 <acad.aryankashyap2812@gmail.com>
2025-08-05 11:47:31 +0530

    wrote some python script

--- Utilities/friend calculator.py
+++ Utilities/friend calculator.py
@@ -15,3 +15,32 @@ Bonus:
 - Give playful advice based on the score range
 - Capitalize and center the final output in a framed box
 """
+
+def friendship_score(name1,name2):
+   name1,name2=name1.lower(),name2.lower()
+   score=0
+   shared_letters=set(name1)&set(name2)
+   vowels=set('aeiou')
+   score+=len(shared_letters)*50
+   score+=len(vowels&shared_letters)*10
+   
+   return min(100,score)
+
+def run_friendship_calculator():
+   print("Friendship Compatibility Calculator")
+   name1=input("Enter friend 1 name: ")
+   name2=input("Enter friend 2 name: ")
+   
+   score=friendship_score(name1,name2)
+   
+   return score
+
+score=run_friendship_calculator()
+print("*"*50)
+
+if score >=75:
+   print(f"You both are {score}% alike.\nYou both are like chai smosa, made for each other.")
+else:
+   print(f"You both are {score}% alike.\nMay be opposite attract each other.")
+
+print("*"*50)   
